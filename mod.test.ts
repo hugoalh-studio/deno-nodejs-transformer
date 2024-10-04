1061872c2d796e8abe577e5267861c36977265f6
hugoalh <hugoalh@gmail.com>
2024-10-04 16:48:47 +0800

    Wrap in test block

--- mod.test.ts
+++ mod.test.ts
@@ -1,41 +1,50 @@
 import { invokeDenoNodeJSTransformer } from "./mod.ts";
-await invokeDenoNodeJSTransformer({
-	assetsCopy: [
-		"LICENSE.md",
-		"README.md"
-	],
-	entrypoints: [{
-		name: ".",
-		path: "mod.ts"
-	}],
-	metadata: {
-		name: "@hugoalh/deno-nodejs-transformer-test",
-		version: "0.1.0",
-		description: "Demo of Deno NodeJS Transformer.",
-		keywords: [
-			"dnt",
-			"test"
+Deno.test("Main", {
+	permissions: {
+		env: true,
+		net: true,
+		read: true,
+		write: true
+	}
+}, async () => {
+	await invokeDenoNodeJSTransformer({
+		assetsCopy: [
+			"LICENSE.md",
+			"README.md"
 		],
-		homepage: "https://github.com/hugoalh-studio/deno-nodejs-transformer#readme",
-		bugs: {
-			url: "https://github.com/hugoalh-studio/deno-nodejs-transformer/issues"
+		entrypoints: [{
+			name: ".",
+			path: "mod.ts"
+		}],
+		metadata: {
+			name: "@hugoalh/deno-nodejs-transformer-test",
+			version: "0.1.0",
+			description: "Demo of Deno NodeJS Transformer.",
+			keywords: [
+				"dnt",
+				"test"
+			],
+			homepage: "https://github.com/hugoalh-studio/deno-nodejs-transformer#readme",
+			bugs: {
+				url: "https://github.com/hugoalh-studio/deno-nodejs-transformer/issues"
+			},
+			license: "MIT",
+			author: "hugoalh",
+			repository: {
+				type: "git",
+				url: "git+https://github.com/hugoalh-studio/deno-nodejs-transformer.git"
+			},
+			scripts: {
+			},
+			engines: {
+				node: ">=16.13.0"
+			},
+			private: false,
+			publishConfig: {
+				access: "public"
+			}
 		},
-		license: "MIT",
-		author: "hugoalh",
-		repository: {
-			type: "git",
-			url: "git+https://github.com/hugoalh-studio/deno-nodejs-transformer.git"
-		},
-		scripts: {
-		},
-		engines: {
-			node: ">=16.13.0"
-		},
-		private: false,
-		publishConfig: {
-			access: "public"
-		}
-	},
-	outputDirectory: "npm",
-	outputDirectoryPreEmpty: true
+		outputDirectory: "npm",
+		outputDirectoryPreEmpty: true
+	});
 });
